c141b9d88a7241ccd65e8fbdbe3e16697473baa9
wq <guddqs@icloud.com>
2019-12-12 14:47:52 +0800

    bug fix

--- env/jnmg-install.sh
+++ env/jnmg-install.sh
@@ -25,13 +25,15 @@ if [[ x"${release}" == x"centos" ]]; then
     sudo yum update -y
     echo '-----------------Update Finished. Start Install-----------------'
     sudo yum install -y java-1.8.0-openjdk.x86_64 nginx maven git
+    sudo systemctl enable nginx
+    sudo systemctl restart nginx
 elif [[ x"${release}" == x"debian" || x"${release}" == x"ubuntu" ]]; then
     sudo apt-get update
     echo '-----------------Update Finished. Start Install-----------------'
     sudo apt-get install -y openjdk-8-jdk nginx maven git
+    sudo systemctl enable nginx
+    sudo systemctl restart nginx
 else
     v=`cat /proc/version`
     echo "Unknown Linux Release: ${v}"
 fi
-systemctl enable nginx
-systemctl start nginx
